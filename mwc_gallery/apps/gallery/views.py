f3c70ed06333f568a0bdebdf9d9eccb2eacc3889
Nolan Brubaker <nolan.brubaker@gmail.com>
2011-08-16 21:51:50 -0400

    Added gallery_list view and document strings.

--- mwc_gallery/apps/gallery/views.py
+++ mwc_gallery/apps/gallery/views.py
@@ -1,26 +1,61 @@
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 from gallery.models import Gallery
 
 def gallery_list(request):
-    pass
+    """
+    Lists the available galleries.
+    """
+    
+    template_name = "gallery/gallery_list.html"
+    
+    # @@@ filter them based on public/private?
+    galleries = Gallery.objects.all()
+    
+    return render_to_response(template_name, {
+        "galleries": galleries,
+    }, context_instance=RequestContext(request))
     
     
 def gallery_view(request):
+    """
+    View a gallery.
+    """
+    
     pass
     
     
 def gallery_create(request):
+    """
+    Create a gallery.
+    """
+    
     pass
     
     
 def gallery_add_photo(request):
+    """
+    Add a photo to the gallery.
+    """
+    
     pass
     
     
 def gallery_remove_photo(request):
+    """
+    Remove a photo from a gallery.
+    """
+    
     pass
     
     
 def gallery_delete(request):
+    """
+    Delete a gallery.
+    """
+    
     pass
     
     

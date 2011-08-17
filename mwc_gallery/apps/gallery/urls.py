ce5241ab63b5ce89ca46eeacdd3643cebb2c2fe5
Nolan Brubaker <nolan.brubaker@gmail.com>
2011-08-16 22:02:29 -0400

    Fixed some typos in module and function names.

--- mwc_gallery/apps/gallery/urls.py
+++ mwc_gallery/apps/gallery/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns("galleries.views",
+urlpatterns = patterns("gallery.views",
     url(r"^$", "gallery_list", name="gallery_list"),
     url(r"create$", "gallery_create", name="gallery_create"),
     url(r"add_photo$", "gallery_add_photo", name="gallery_add_photo"),
-    url(r"remove_photo$", "gallery_remove_phot", name="gallery_remove_photo"),
+    url(r"remove_photo$", "gallery_remove_photo", name="gallery_remove_photo"),
     url(r"delete$", "gallery_delete", name="gallery_delete"),
 )

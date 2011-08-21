58a4bc8afc580280cc4f9c56265c2a66545fba43
Nolan Brubaker <nolan.brubaker@gmail.com>
2011-08-21 01:31:34 -0400

    Fixed regular expressions on urls.

--- mwc_gallery/apps/gallery/urls.py
+++ mwc_gallery/apps/gallery/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns("gallery.views",
     url(r"^$", "gallery_list", name="gallery_list"),
-    url(r"create$", "gallery_create", name="gallery_create"),
-    url(r"add_photo$", "gallery_add_photo", name="gallery_add_photo"),
-    url(r"remove_photo$", "gallery_remove_photo", name="gallery_remove_photo"),
-    url(r"delete$", "gallery_delete", name="gallery_delete"),
+    url(r"^create$", "gallery_create", name="gallery_create"),
+    url(r"^add_photo$", "gallery_add_photo", name="gallery_add_photo"),
+    url(r"^remove_photo$", "gallery_remove_photo", name="gallery_remove_photo"),
+    url(r"^delete$", "gallery_delete", name="gallery_delete"),
 )

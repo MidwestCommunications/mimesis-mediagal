5c800a90bbfb10e495bb4adad37837e5ae407924
Nolan Brubaker <nolan.brubaker@gmail.com>
2011-08-25 23:04:09 -0400

    Corrected test case.
    The test case was checking for completely invalid file names, which I'm
    not sure how to create on the file system.

--- mwc_gallery/apps/gallery/tests/file.py
+++ mwc_gallery/apps/gallery/tests/file.py
@@ -14,7 +14,7 @@ class ZipFileTest(TestCase):
     file_names = ["f1.jpg","f2.png","f3.gif"]
     def setUp(self):
         mkdir(SCRATCH_DIR)
-        z = ZipFile(SCRATCH_DIR + "ztest.zip")
+        z = ZipFile(SCRATCH_DIR + "ztest.zip", "w")
         # Create files to be zipped.
         for f_name in self.file_names:
             f = open(SCRATCH_DIR + f_name, "w")
@@ -22,16 +22,16 @@ class ZipFileTest(TestCase):
             f.close()
             z.write(SCRATCH_DIR + f_name)
         z.close()
-
+        
     def tearDown(self):
         rmtree(SCRATCH_DIR)
-
-class ZipFileCheckTest(TestCase):
-
+        
+class ZipFileCheckTest(ZipFileTest):
+    
     def test_check_zip_file(self):
         names = _check_zip_file(SCRATCH_DIR + "ztest.zip")
-        self.assertTrue("/test.jpg" not in names)
-        self.assertTrue("a/...jpg" not in names)
+        self.assertTrue("tmp/f1.jpg" in names)
+        self.assertTrue("tmp/f2.png" in names)
 
 
 

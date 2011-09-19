27648d2627f941b2c64edadf73c347dda5d17a08
Nolan Brubaker <nolan.brubaker@gmail.com>
2011-09-19 18:00:54 -0400

    Formatting cleanup.

--- mwc_gallery/apps/gallery/views.py
+++ mwc_gallery/apps/gallery/views.py
@@ -7,43 +7,45 @@ from django.contrib.auth.decorators import login_required
 from gallery.forms import MediaFormSet, GalleryDetailsForm
 from gallery.models import Gallery, GalleryMedia
 
+
 def gallery_list(request):
     """
     Lists the available galleries.
     """
-    
+
     template_name = "gallery/gallery_list.html"
-    
+
     # @@@ filter them based on public/private?
     galleries = Gallery.objects.all()
-    
+
     return render_to_response(template_name, {
         "galleries": galleries,
     }, context_instance=RequestContext(request))
-    
-    
+
+
 def gallery_details(request, gallery_id):
     """
     View a gallery.
     """
-    
+
     template_name = "gallery/gallery_details.html"
-    
+
     gallery = get_object_or_404(Gallery, pk=gallery_id)
-    
-    medias = gallery.media.all()
-    
+
+    media = gallery.media.all()
+
     return render_to_response(template_name, {
         "gallery": gallery,
         "media": media,
     }, context_instance=RequestContext(request))
-    
+
+
 @login_required
 def gallery_create(request):
     """
     Create a gallery.
     """
-    
+
     template_name = "gallery/gallery_create.html"
     if request.method == "POST":
         media_formset = MediaFormSet(request.POST, request.FILES)
@@ -77,38 +79,36 @@ def gallery_create(request):
             pass
     else:
         initial_media_data = [{"creator": request.user.pk}]
-        
+
         media_formset = MediaFormSet(initial=initial_media_data)
 
         gallery_form = GalleryDetailsForm()
-        
-    
+
     return render_to_response(template_name, {
         "gallery_form": gallery_form,
         "media_formset": media_formset
     }, context_instance=RequestContext(request))
-    
+
+
 def gallery_add_media(request):
     """
     Add a media to the gallery.
     """
-    
+
     pass
-    
-    
+
+
 def gallery_remove_media(request):
     """
     Remove a media from a gallery.
     """
-    
+
     pass
-    
-    
+
+
 def gallery_delete(request):
     """
     Delete a gallery.
     """
-    
+
     pass
-    
-    

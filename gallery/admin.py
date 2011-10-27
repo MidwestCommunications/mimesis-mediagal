b6ac0f34df6204600572661dc75ee0da0824aeb8
Nolan Brubaker <nolan@nbrubaker.com>
2011-10-27 06:59:26 -0400

    Clean up import paths from example application.

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from apps.gallery.models import Gallery, GalleryMedia, GallerySites
+from gallery.models import Gallery, GalleryMedia, GallerySites
 
 class GalleryMediaInline(admin.TabularInline):
     model = GalleryMedia

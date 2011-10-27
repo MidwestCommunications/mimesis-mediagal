3352631f96dea99c9438f39afefda4494927d07f
Nolan Brubaker <nolan@nbrubaker.com>
2011-10-27 01:46:43 -0400

    Make intermediary models admin inlines.

--- mwc_gallery/apps/gallery/admin.py
+++ mwc_gallery/apps/gallery/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from apps.gallery.models import Gallery, GalleryMedia, GallerySites
 
-admin.site.register(Gallery)
-admin.site.register(GalleryMedia)
-admin.site.register(GallerySites)
+class GalleryMediaInline(admin.TabularInline):
+    model = GalleryMedia
+    
+    
+class GallerySitesInline(admin.StackedInline):
+    model = GallerySites
+    
+    
+class GalleryAdmin(admin.ModelAdmin):
+    inlines = [GalleryMediaInline, GallerySitesInline]
+    
+
+admin.site.register(Gallery, GalleryAdmin)

b413f1f162b7397e391aa402e892650b7c229ead
Nolan Brubaker <nolan@nbrubaker.com>
2011-11-04 00:38:59 -0400

    Add runtest script.

---
+++ runtests.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from os.path import dirname, abspath
+from os import environ
+import sys
+
+environ["DJANGO_SETTINGS_MODULE"] = "example_project.settings"
+
+from django.conf import settings as django_settings
+
+setattr(django_settings, "DATABASE_ENGINE", "sqlite3")
+
+def runtests(*test_args):
+    if not test_args:
+        test_args = ["gallery"]
+    parent = dirname(abspath(__file__))
+    sys.path.insert(0, parent)
+    from django.test.simple import run_tests
+    failures = run_tests(test_args, verbosity=1, interactive=True)
+    sys.exit(failures)
+    
+if __name__ == "__main__":
+    runtests(*sys.argv[1:])

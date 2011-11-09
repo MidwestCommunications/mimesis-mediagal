2e8e2a6cdc92304a4da843857249c2eb6273dbb5
Nolan Brubaker <nolan@nbrubaker.com>
2011-11-08 21:31:18 -0500

    Add tests for gallery_create view.

--- gallery/tests/views.py
+++ gallery/tests/views.py
@@ -15,12 +15,14 @@ from gallery.models import Gallery
 class GalleryViewTest(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create(username="test", password="test")
+        self.user = User.objects.create_user(username="test", email="test_email@address.com", password="test")
         self.g = Gallery.objects.create(name="Test gallery", owner=self.user)
         
         test_file = File(open(join(django_settings.MEDIA_ROOT, "test.jpg")))
         self.media = MediaUpload.objects.create(caption="", media=test_file, creator=self.user)
         test_file.close()
+        
+        self.client.login(username="test", password="test")
         
     def test_gallery_list_template(self):
         url = reverse("gallery_list")
@@ -48,3 +50,15 @@ class GalleryViewTest(TestCase):
         self.assertTrue("gallery" in response.context)
         self.assertTrue("media" in response.context)
         self.assertTrue("thumbnail_sizes" in response.context)
+        
+    def test_gallery_create_template(self):
+        url = reverse("gallery_create")
+        response = self.client.get(url)
+        
+        self.assertTemplateUsed(response, "gallery/gallery_create.html")
+        
+    def test_gallery_create_context(self):
+        url = reverse("gallery_create")
+        response = self.client.get(url)
+        
+        self.assertTrue("gallery_form" in response.context)

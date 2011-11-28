56766aa5ef5fb8ccd86946864d38a490eb7ccc74
James Lecker <jlecker@gmail.com>
2011-11-28 14:09:17 -0600

    add trailing slashes to URLs, per convention

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns("gallery.views",
     url(r"^$", "gallery_list", name="gallery_list"),
-    url(r"^(\d+)$", "gallery_details", name="gallery_details"),
-    url(r"^create$", "gallery_create", name="gallery_create"),
-    url(r"^add_media/(\d+)$", "gallery_add_media", name="gallery_add_media"),
-    url(r"^delete$", "gallery_delete", name="gallery_delete"),
-    url(r"edit_details/(\d+)$", "gallery_edit_details", name="gallery_edit_details"),
-    url(r"^(\d+)/image_details/(\d+)$", "gallery_image_details", name="gallery_image_details"),
+    url(r"^(\d+)/$", "gallery_details", name="gallery_details"),
+    url(r"^create/$", "gallery_create", name="gallery_create"),
+    url(r"^add_media/(\d+)/$", "gallery_add_media", name="gallery_add_media"),
+    url(r"^delete/$", "gallery_delete", name="gallery_delete"),
+    url(r"edit_details/(\d+)/$", "gallery_edit_details", name="gallery_edit_details"),
+    url(r"^(\d+)/image_details/(\d+)/$", "gallery_image_details", name="gallery_image_details"),
 )

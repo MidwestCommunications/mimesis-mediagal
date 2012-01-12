1c9485853050b6baad4f30e7d06869d0b6197ef3
James Lecker <jlecker@gmail.com>
2012-01-12 12:27:20 -0600

    add migrations and templates for mediagal

---
+++ mediagal/migrations/0001_initial.py
@@ -0,0 +1,165 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Gallery'
+        db.create_table('mediagal_gallery', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=250)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('owner', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('updated', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('cover', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['mimesis.MediaUpload'], null=True)),
+        ))
+        db.send_create_signal('mediagal', ['Gallery'])
+
+        # Adding model 'GalleryMedia'
+        db.create_table('mediagal_gallerymedia', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('gallery', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['mediagal.Gallery'])),
+            ('media', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['mimesis.MediaUpload'])),
+        ))
+        db.send_create_signal('mediagal', ['GalleryMedia'])
+
+        # Adding model 'GallerySites'
+        db.create_table('mediagal_gallerysites', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('gallery', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['mediagal.Gallery'])),
+            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
+        ))
+        db.send_create_signal('mediagal', ['GallerySites'])
+
+        # Adding model 'GalleryAssociation'
+        db.create_table('mediagal_galleryassociation', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('gallery', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['mediagal.Gallery'])),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
+        ))
+        db.send_create_signal('mediagal', ['GalleryAssociation'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Gallery'
+        db.delete_table('mediagal_gallery')
+
+        # Deleting model 'GalleryMedia'
+        db.delete_table('mediagal_gallerymedia')
+
+        # Deleting model 'GallerySites'
+        db.delete_table('mediagal_gallerysites')
+
+        # Deleting model 'GalleryAssociation'
+        db.delete_table('mediagal_galleryassociation')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'mediagal.gallery': {
+            'Meta': {'ordering': "['-updated']", 'object_name': 'Gallery'},
+            'cover': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mimesis.MediaUpload']", 'null': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'media': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'galleries'", 'symmetrical': 'False', 'through': "orm['mediagal.GalleryMedia']", 'to': "orm['mimesis.MediaUpload']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['sites.Site']", 'null': 'True', 'through': "orm['mediagal.GallerySites']", 'symmetrical': 'False'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'})
+        },
+        'mediagal.galleryassociation': {
+            'Meta': {'object_name': 'GalleryAssociation'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'gallery': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mediagal.Gallery']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'mediagal.gallerymedia': {
+            'Meta': {'object_name': 'GalleryMedia'},
+            'gallery': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mediagal.Gallery']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'media': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mimesis.MediaUpload']"})
+        },
+        'mediagal.gallerysites': {
+            'Meta': {'object_name': 'GallerySites'},
+            'gallery': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mediagal.Gallery']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"})
+        },
+        'mimesis.mediaupload': {
+            'Meta': {'object_name': 'MediaUpload'},
+            'caption': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'media': ('django.db.models.fields.files.FileField', [], {'max_length': '500'}),
+            'media_subtype': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'media_type': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'taggit.tag': {
+            'Meta': {'object_name': 'Tag'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'})
+        },
+        'taggit.taggeditem': {
+            'Meta': {'object_name': 'TaggedItem'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_tagged_items'", 'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taggit_taggeditem_items'", 'to': "orm['taggit.Tag']"})
+        }
+    }
+
+    complete_apps = ['mediagal']
